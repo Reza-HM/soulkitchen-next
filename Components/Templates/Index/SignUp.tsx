3048c8901e43efd6ed757595a90474d795499cb8
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-23 11:58:29 +0330

    added neumorphic styles to the project

--- Components/Templates/Index/SignUp.tsx
+++ Components/Templates/Index/SignUp.tsx
@@ -1,28 +1,28 @@
 const SignUp = () => {
   return (
     <div className="container p-20 mt-40">
-      <div className="bg-zinc-200/40 p-20 flex flex-col items-center gap-12">
+      <div className="bg-zinc-200/40 p-20 flex flex-col items-center gap-12 neumorphic-container">
         <h2 className="font-PlayfairDisplay text-5xl font-semibold tracking-wider">
           Keep Up to Date with Soul Kitchen
         </h2>
-        <p>
+        <p className="">
           Sign up to be the first to receive special news and event updates from
           SoulKitchen.
         </p>
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4 neumorphic-form">
           <form action="" className="flex gap-4">
             <input
               type="text"
-              className="p-4 border-1 !border-zinc-800 text-zinc-700 rounded-xl"
+              className="p-4 border-1 !border-zinc-800 text-zinc-700 rounded-xl neumorphic-input"
               placeholder="ENTER EMAIL"
             />
             <input
               type="password"
-              className="p-4 border-1 !border-zinc-800 text-zinc-700 rounded-xl"
+              className="p-4 border-1 !border-zinc-800 text-zinc-700 rounded-xl neumorphic-input"
               placeholder="ENTER PASSWORD"
             />
           </form>
-          <button className="bg-black text-zinc-400 hover:bg-zinc-500 hover:text-white w-full p-4 rounded-xl duration-300">
+          <button className="neumorphic-button !bg-black !text-zinc-500 !w-full">
             SIGN UP
           </button>
         </div>
