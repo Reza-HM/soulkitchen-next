dfa6a96a33abcbdfe90eac0b46a9da0257140dba
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-23 09:41:08 +0330

    adding hover effect to gallery images

--- Components/Templates/Index/Gallery.tsx
+++ Components/Templates/Index/Gallery.tsx
@@ -5,53 +5,85 @@ const Gallery = () => {
     <div className="container px-0 mt-40">
       <div className="flex justify-center items-center gap-2">
         <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
-          <Image
-            alt="gallery photo 1"
-            src="/img/gallery1.jpg"
-            width={800}
-            height={800}
-          />
+          <div className="group relative cursor-pointer">
+            <Image
+              alt="gallery photo 1"
+              src="/img/gallery1.jpg"
+              width={800}
+              height={800}
+              className="group-hover:brightness-50 duration-300"
+            />
+            <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+              SPICED POACHED PEARS
+            </p>
+          </div>
+
           <div className="flex justify-between items-center gap-2">
-            <Image
-              alt=""
-              className="flex-1"
-              src="/img/gallery2.jpg"
-              width={200}
-              height={200}
-            />
-            <Image
-              alt=""
-              className="flex-1"
-              src="/img/gallery3.jpg"
-              width={200}
-              height={200}
-            />
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery2.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                VEGAN PRALINES
+              </p>
+            </div>
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery3.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                COCONUT PARFAIT WITH BERRY SAUCE
+              </p>
+            </div>
           </div>
         </div>
         <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
           <div className="flex items-center justify-between gap-2">
-            <Image
-              alt=""
-              className="flex-1"
-              src="/img/gallery5.jpg"
-              width={200}
-              height={200}
-            />
-            <Image
-              alt=""
-              className="flex-1"
-              src="/img/gallery6.jpg"
-              width={200}
-              height={200}
-            />
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery5.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                LEMON PARFAIT
+              </p>
+            </div>
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery6.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                SPRING SALAD
+              </p>
+            </div>
           </div>
-          <Image
-            alt=""
-            src="/img/gallery4.jpg"
-            width={800}
-            height={800}
-            className="!h-[900px]"
-          />
+          <div className="group relative cursor-pointer flex-1">
+            <Image
+              alt=""
+              src="/img/gallery4.jpg"
+              width={800}
+              height={800}
+              className="!h-[900px] flex-1 group-hover:brightness-50 duration-300"
+            />
+            <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+              SCALLOPS WITH ORANGE BUTTER
+            </p>
+          </div>
         </div>
       </div>
     </div>
