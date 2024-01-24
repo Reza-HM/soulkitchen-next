e42e1afb153f5c928f753bd4669654d873f9c1f7
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-24 12:59:48 +0330

    coded chef's section at aboutus page

--- pages/aboutus/index.tsx
+++ pages/aboutus/index.tsx
@@ -1,9 +1,11 @@
+import Chef from "@/Components/Templates/Aboutus/Chef";
 import Header from "@/Components/Templates/Aboutus/Header";
 
 const Index = () => {
   return (
     <div>
       <Header />
+      <Chef />
     </div>
   );
 };
