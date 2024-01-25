1389a606629eb66a681a7a6cd803dac6b02529b0
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-25 18:50:03 +0330

    coded team page's service manager section

--- Components/Templates/Team/ServiceManager.tsx
+++ Components/Templates/Team/ServiceManager.tsx
@@ -1,6 +1,37 @@
+import Image from "next/image";
+
 const ServiceManager = () => {
   return (
-    <div>ServiceManager</div>
-  )
-}
-export default ServiceManager
+    <div className="flex justify-between items-center">
+      <div className="flex-1 flex flex-col">
+        <h2 className="text-6xl font-PlayfairDisplay_Italic font-bold text-end py-6 px-20">
+          Gwyn Lee
+        </h2>
+        <p className="line-clamp-5 leading-loose text-2xl text-end py-6 px-20">
+          Gwyn is passionate about food, wine and service and basically
+          everything to do with the restaurant industry. Minty bananas soba
+          noodles chia seeds green tea.
+        </p>
+        <p className="line-clamp-5 leading-loose text-2xl text-end py-6 px-20">
+          Seasonal chili pepper green papaya salad apricot Mexican fiesta
+          avocado banana bread almond milk chai latte dill ghost pepper mangos
+          green pepper tabasco pepper ultra creamy avocado lime.
+        </p>
+        <p className="line-clamp-5 leading-loose text-2xl text-end py-6 px-20">
+          FAVORITE FOOD: ROASTED KING CRAB WITH UNI SERRANO
+        </p>
+      </div>
+
+      <div className="flex-1">
+        <Image
+          alt="about us header's photo"
+          src="/img/servicemanager.jpg"
+          className="!w-full h-full object-cover object-center !grayscale"
+          width={800}
+          height={800}
+        />
+      </div>
+    </div>
+  );
+};
+export default ServiceManager;
