ec9c9b992a16ffaf8ff948a8bed09bf3688a19e3
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-26 20:52:54 +0330

    coded the faq page

---
+++ Components/Templates/Faq/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center !h-[60vh] relative bg-[url('/img/faq.jpg')] bg-cover bg-center bg-no-repeat brightness-parent">
+      <div className="flex flex-col gap-8 !z-50">
+        <h1 className="text-white text-8xl font-PlayfairDisplay">
+         FAQ
+        </h1>
+      </div>
+    </div>
+  );
+};
+export default Header;
---
+++ pages/faq/index.tsx
@@ -0,0 +1,12 @@
+import Accordion from "@/Components/Templates/Faq/Accordion";
+import Header from "@/Components/Templates/Faq/Header";
+
+const index = () => {
+  return (
+    <div>
+      <Header />
+      <Accordion answer="" question="" />
+    </div>
+  );
+};
+export default index;
