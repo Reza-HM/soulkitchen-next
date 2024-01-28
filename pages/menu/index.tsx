00c09999f69681bc006692178c740db0c8928df5
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-28 20:47:09 +0330

    coded the menu page

---
+++ pages/menu/index.tsx
@@ -0,0 +1,12 @@
+import DrinksMenu from "@/Components/Templates/Index/DrinksMenu";
+import Menu from "@/Components/Templates/Index/Menu";
+
+const index = () => {
+  return (
+    <div>
+      <Menu />
+      <DrinksMenu />
+    </div>
+  );
+};
+export default index;
