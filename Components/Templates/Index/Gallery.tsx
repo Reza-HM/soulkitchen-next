94b6681f65063a42043491b7f8baf9ea8e0eecf3
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-29 11:30:35 +0330

    responsived gallery at index page

--- Components/Templates/Index/Gallery.tsx
+++ Components/Templates/Index/Gallery.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Gallery = () => {
   return (
     <div className="container px-0 mt-40">
-      <div className="flex justify-center items-center gap-2">
+      <div className="flex justify-center flex-wrap gap-2">
         <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
           <div className="group relative cursor-pointer">
             <Image
@@ -77,7 +77,7 @@ const Gallery = () => {
               src="/img/gallery4.jpg"
               width={800}
               height={800}
-              className="!h-[900px] flex-1 group-hover:brightness-50 duration-300"
+              className="!h-auto md:!h-[450px] lg:!h-[600px] xl:!h-[745px] 2xl:!h-[842px] flex-1 group-hover:brightness-50 duration-300"
             />
             <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
               SCALLOPS WITH ORANGE BUTTER
