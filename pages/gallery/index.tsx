4f1dcc6c1dd0a0cfa9e7262ce5d67c284d616ee5
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-29 12:56:19 +0330

    refactored and responsived the gallery page

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -11,8 +11,8 @@ const Gallery = () => {
           Gallery
         </h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-32 mb-20">
-        <div className="w-full flex flex-col gap-2">
+      <div className="flex justify-center flex-wrap gap-2 mt-40 mb-2">
+        <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
           <div className="group relative cursor-pointer">
             <Image
               alt="gallery photo 1"
@@ -52,7 +52,7 @@ const Gallery = () => {
             </div>
           </div>
         </div>
-        <div className="w-full flex flex-col gap-2">
+        <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
           <div className="flex items-center justify-between gap-2">
             <div className="group relative cursor-pointer flex-1">
               <Image
@@ -85,54 +85,16 @@ const Gallery = () => {
               src="/img/gallery4.jpg"
               width={800}
               height={800}
-              className="!h-[900px] flex-1 group-hover:brightness-50 duration-300"
+              className="!h-auto md:!h-[450px] lg:!h-[600px] xl:!h-[745px] 2xl:!h-[842px] flex-1 group-hover:brightness-50 duration-300"
             />
             <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
               SCALLOPS WITH ORANGE BUTTER
             </p>
           </div>
         </div>
-        <div className="w-full flex flex-col gap-2">
-          <div className="flex items-center justify-between gap-2">
-            <div className="group relative cursor-pointer flex-1">
-              <Image
-                alt=""
-                className="flex-1 group-hover:brightness-50 duration-300"
-                src="/img/gallery5.jpg"
-                width={400}
-                height={400}
-              />
-              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
-                LEMON PARFAIT
-              </p>
-            </div>
-            <div className="group relative cursor-pointer flex-1">
-              <Image
-                alt=""
-                className="flex-1 group-hover:brightness-50 duration-300"
-                src="/img/gallery6.jpg"
-                width={400}
-                height={400}
-              />
-              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
-                SPRING SALAD
-              </p>
-            </div>
-          </div>
-          <div className="group relative cursor-pointer flex-1">
-            <Image
-              alt=""
-              src="/img/gallery4.jpg"
-              width={800}
-              height={800}
-              className="!h-[900px] flex-1 group-hover:brightness-50 duration-300"
-            />
-            <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
-              SCALLOPS WITH ORANGE BUTTER
-            </p>
-          </div>
-        </div>
-        <div className="w-full flex flex-col gap-2">
+      </div>
+      <div className="flex justify-center flex-wrap gap-2 mb-40">
+        <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
           <div className="group relative cursor-pointer">
             <Image
               alt="gallery photo 1"
@@ -170,6 +132,46 @@ const Gallery = () => {
                 COCONUT PARFAIT WITH BERRY SAUCE
               </p>
             </div>
+          </div>
+        </div>
+        <div className="w-full md:w-1/3 md:flex-1 flex flex-col gap-2">
+          <div className="flex items-center justify-between gap-2">
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery5.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                LEMON PARFAIT
+              </p>
+            </div>
+            <div className="group relative cursor-pointer flex-1">
+              <Image
+                alt=""
+                className="flex-1 group-hover:brightness-50 duration-300"
+                src="/img/gallery6.jpg"
+                width={400}
+                height={400}
+              />
+              <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+                SPRING SALAD
+              </p>
+            </div>
+          </div>
+          <div className="group relative cursor-pointer flex-1">
+            <Image
+              alt=""
+              src="/img/gallery4.jpg"
+              width={800}
+              height={800}
+              className="!h-auto md:!h-[450px] lg:!h-[600px] xl:!h-[745px] 2xl:!h-[842px] flex-1 group-hover:brightness-50 duration-300"
+            />
+            <p className="absolute text-center inset-[50%] -translate-x-1/2 -translate-y-1/2 w-96 tracking-widest text-2xl text-white hidden group-hover:block">
+              SCALLOPS WITH ORANGE BUTTER
+            </p>
           </div>
         </div>
       </div>
