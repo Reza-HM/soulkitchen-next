525b04006f42ca692bdca45b6a13b07517972a2c
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-29 09:50:47 +0330

    responsived the header of index page

--- Components/Templates/Index/Header.tsx
+++ Components/Templates/Index/Header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-between gap-8 !z-40">
-      <div className="flex-1 flex justify-center w-full md:w-1/3 bg-[url('/img/headerImg-1.jpg')] bg-cover bg-center bg-no-repeat animate-fade-up bg-blend-darken bg-black/30 cursor-pointer">
+    <div className="flex flex-wrap justify-center lg:justify-between gap-8 !z-40">
+      <div className="w-full md:w-1/3 md:flex-1 flex justify-center bg-[url('/img/headerImg-1.jpg')] bg-cover bg-center bg-no-repeat animate-fade-up bg-blend-darken bg-black/30 cursor-pointer !h-[50rem] !min-h-[50rem] order-2 md:order-1">
         <div className="flex flex-col items-center justify-end mb-12 text-white gap-4">
-          <h3 className="font-PlayfairDisplay_Italic text-5xl font-bold tracking-widest">
+          <h3 className="font-PlayfairDisplay_Italic text-3xl sm:text-5xl font-bold tracking-widest text-center">
             Lunch Special
           </h3>
-          <p className="font-bold text-3xl">See Menu</p>
+          <p className="font-bold text-xl sm:text-3xl text-center">See Menu</p>
         </div>
       </div>
-      <div className="flex-1 relative md:w-1/3 animate-fade-up bg-blend-darken bg-black/30 cursor-pointer">
+      <div className="w-full md:w-1/3 md:flex-1 relative animate-fade-up bg-blend-darken bg-black/30 cursor-pointer !h-[50rem] !min-h-[50rem] order-1 md:order-2">
         <video
           src="/img/header-video.mp4"
           className="inset-0 w-full h-full object-cover object-center"
@@ -20,12 +20,12 @@ const Header = () => {
           playsInline
           autoPlay
         ></video>
-        <div className="flex flex-col items-center text-white gap-8 absolute inset-0 top-40">
+        <div className="flex flex-col items-center text-white gap-8 absolute inset-0 top-40 p-2">
           <h3>TASTE THE DIFFERENCE</h3>
-          <h2 className="text-5xl font-PlayfairDisplay_Black text-center leading-relaxed">
+          <h2 className="text-5xl font-PlayfairDisplay_Black text-center leading-relaxed p-2">
             THE SOUL KITCHEN EXPERIENCE
           </h2>
-          <p className="text-center max-w-md">
+          <p className="text-center max-w-md p-2">
             MONDAY thru FRIDAY 5pm – 11pm SATURDAY 3pm – 11pm SUNDAY closed BOOK
           </p>
           <Link
@@ -36,12 +36,14 @@ const Header = () => {
           </Link>
         </div>
       </div>
-      <div className="flex-1 flex justify-center w-full md:w-1/3 bg-[url('/img/headerImg-2.jpg')] bg-cover bg-center bg-no-repeat animate-fade-up bg-blend-darken bg-black/30 cursor-pointer">
+      <div className="w-full md:w-1/3 md:flex-1 flex justify-center bg-[url('/img/headerImg-2.jpg')] bg-cover bg-center bg-no-repeat animate-fade-up bg-blend-darken bg-black/30 cursor-pointer !h-[50rem] !min-h-[50rem] order-3 md:order-3">
         <div className="flex flex-col items-center justify-end mb-12 text-white gap-4">
-          <h3 className="font-PlayfairDisplay_Italic text-5xl font-bold tracking-widest">
+          <h3 className="font-PlayfairDisplay_Italic text-3xl sm:text-5xl font-bold tracking-widest text-center">
             Soul Kitchen Malibu{" "}
           </h3>
-          <p className="font-bold text-3xl">Book Your Table </p>
+          <p className="font-bold text-xl sm:text-3xl text-center">
+            Book Your Table{" "}
+          </p>
         </div>
       </div>
     </div>
