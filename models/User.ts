7e4a778c8b716dcef6ff05ff7582712143808fd7
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-30 10:55:00 +0330

    next backend => coded the signup api

--- models/User.ts
+++ models/User.ts
@@ -1,19 +1,35 @@
 import mongoose, { Schema, Document } from "mongoose";
+import { hashPassword } from "@/utils/auth";
 
-interface IUser extends Document {
+export interface IUser extends Document {
   username: string;
   email: string;
+  phone: string;
   password: string;
+  token?: string;
+  role?: string;
 }
 
 const userSchema: Schema<IUser> = new Schema(
   {
     username: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
     password: { type: String, required: true },
+    token: { type: String },
+    role: { type: String },
   },
   { timestamps: true }
 );
+
+userSchema.pre<IUser>("save", async function (next) {
+  // Hash the password before saving
+  if (this.isModified("password")) {
+    this.password = await hashPassword(this.password);
+  }
+
+  next();
+});
 
 const UserModel =
   (mongoose.models.User as mongoose.Model<IUser>) ||
---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,85 @@
+import connectToDB from "@/configs/db";
+import UserModel, { IUser } from "@/models/User";
+import { generateToken, hashPassword } from "@/utils/auth";
+import { serialize } from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface SignUpRequestBody {
+  username: string;
+  email: string;
+  phone: string;
+  password: string;
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  try {
+    connectToDB();
+    const { username, email, phone, password } = req.body as SignUpRequestBody;
+
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(422).json({ message: "Invalid email format" });
+    }
+
+    const phoneRegex: RegExp = /^09\d{9}$/;
+    if (!phoneRegex.test(phone)) {
+      return res.status(422).json({ message: "Invalid phone number format" });
+    }
+
+    if (password.trim().length < 8) {
+      return res
+        .status(422)
+        .json({ message: "Password must be at least 8 characters long" });
+    }
+
+    const usernameRegex: RegExp = /^[a-zA-Z0-6_]+$/;
+    if (!usernameRegex.test(username)) {
+      return res.status(422).json({ message: "Invalid username format" });
+    }
+
+    const doesUserExists = await UserModel.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (doesUserExists) {
+      return res
+        .status(422)
+        .json({ message: "This Username Or Email Already Exists." });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const token = generateToken({ email });
+
+    const users = await UserModel.find({});
+
+    await UserModel.create({
+      username,
+      email,
+      phone,
+      password: hashedPassword,
+      token,
+      role: users.length > 0 ? "USER" : "ADMIN",
+    } as IUser);
+
+    return res
+      .setHeader(
+        "Set-Cookie",
+        serialize("token", token, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 60 * 24 * 3,
+        })
+      )
+      .status(201)
+      .json({ message: "You Signed Up Successfully", token });
+  } catch (err) {
+    return res.status(500).json({ message: "Unknown Internal Server Error." });
+  }
+};
+
+export default handler;
