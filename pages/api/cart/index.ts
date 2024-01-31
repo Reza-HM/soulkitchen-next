6a5bcc0ccc9ce76066c05647ee610ec67b750a83
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-31 11:46:41 +0330

    coded the delete and put api for shopping cart

--- pages/api/cart/index.ts
+++ pages/api/cart/index.ts
@@ -20,7 +20,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       await updateShoppingCart(req, res);
       break;
     case "DELETE":
-      await deleteShoppingCart(req, res);
+      await removeFromShoppingCart(req, res);
       break;
     default:
       res.status(405).json({ message: "Method Not Allowed." });
@@ -90,6 +90,75 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(200).json(user.shoppingCart);
     } catch (err) {
       console.error(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  async function updateShoppingCart(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { userId, productId, quantity } = req.body;
+
+      if (!userId || !productId || !quantity) {
+        return res
+          .status(400)
+          .json({ message: "User ID, Product ID, and Quantity are required" });
+      }
+
+      const user = await UserModel.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const cartItem = user.shoppingCart.find(
+        (item) => String(item.productId) === productId
+      );
+
+      if (cartItem) {
+        cartItem.quantity = quantity;
+        await user.save();
+        res.status(200).json(user.shoppingCart);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Item not found in the shopping cart" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  async function removeFromShoppingCart(
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    try {
+      const { userId, productId } = req.body;
+
+      if (!userId || !productId) {
+        return res
+          .status(400)
+          .json({ message: "User ID and Product ID are required" });
+      }
+
+      const user = await UserModel.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const filteredCart = user.shoppingCart.filter(
+        (item) => String(item.productId) !== productId
+      );
+
+      user.shoppingCart = filteredCart;
+
+      await user.save();
+
+      res.status(200).json(user.shoppingCart);
+    } catch (error) {
+      console.error(error);
       res.status(500).json({ message: "Internal Server Error" });
     }
   }
