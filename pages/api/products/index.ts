ff8201811171ecaf10cb28de55a4894af2a2b6bf
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-31 12:48:14 +0330

    coded the get and post apis for product

---
+++ pages/api/products/index.ts
@@ -0,0 +1,44 @@
+import connectToDB from "@/configs/db";
+import ProductModel from "@/models/Product";
+import { NextApiRequest, NextApiResponse } from "next";
+
+connectToDB();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    try {
+      const products = await ProductModel.find();
+      return res.status(200).json(products);
+    } catch (err) {
+      console.error("Error fetching products:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const { name, description, price, img, shortName } = req.body;
+
+      if (!name || !description || !price || !img || !shortName) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+
+      const newProduct = new ProductModel({
+        name,
+        description,
+        price,
+        img,
+        shortName,
+      });
+
+      const savedProduct = await newProduct.save();
+
+      res.status(201).json({ message: "New Product Created:", savedProduct });
+    } catch (err) {
+      console.error("Error creating product:", err);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
+  }
+};
+
+export default handler;
