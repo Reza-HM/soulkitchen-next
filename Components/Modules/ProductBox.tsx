9ebf81af858467581c27e9d67288ba0f997b25b7
Reza Hamzei <hamzeiamir205@gmail.com>
2024-01-31 13:43:18 +0330

    used the products get api in the frontend

--- Components/Modules/ProductBox.tsx
+++ Components/Modules/ProductBox.tsx
@@ -1,8 +1,17 @@
+import { IProduct } from "@/models/Product";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { FC } from "react";
 
-const ProductBox = () => {
+interface ProductBoxProps {
+  name: string;
+  price: string;
+  img: string;
+  shortName: string;
+}
+
+const ProductBox: FC<ProductBoxProps> = ({ name, shortName, price, img }) => {
   const router = useRouter();
 
   return (
@@ -11,18 +20,13 @@ const ProductBox = () => {
         ? "col-span-12 sm:col-span-6 md:col-span-3"
         : "")} flex flex-col items-center justify-center gap-8 p-8`}
     >
-      <Link href="/product/1">
-        <Image
-          alt="product's img"
-          src="/img/product1.jpg"
-          width={300}
-          height={300}
-        />
+      <Link href={`/product/${shortName}`}>
+        <Image alt="product's img" src={img} width={300} height={300} />
       </Link>
-      <Link href="/product/1">
-        <span className="text-2xl tracking-widest">OLIVE OIL</span>
+      <Link href={`/product/${shortName}`}>
+        <span className="text-2xl tracking-widest">{name}</span>
       </Link>
-      <span className="text-2xl tracking-widest">$17.00</span>
+      <span className="text-2xl tracking-widest">${price}</span>
       <Link
         href="/cart"
         className="opacity-0 invisible group-hover:opacity-100 group-hover:visible animate-fade-up transition-double"
--- pages/allProducts/index.tsx
+++ pages/allProducts/index.tsx
@@ -1,6 +1,14 @@
 import ProductBox from "@/Components/Modules/ProductBox";
+import connectToDB from "@/configs/db";
+import ProductModel, { IProduct } from "@/models/Product";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { FC } from "react";
 
-const AllProducts = () => {
+interface AllProductsProps {
+  products: IProduct[];
+}
+
+const AllProducts: FC<AllProductsProps> = ({ products }) => {
   return (
     <div className="container p-20">
       <h2 className="font-bold text-3xl sm:text-5xl md:text-7xl tracking-widest text-center my-12 ">
@@ -15,24 +23,26 @@ const AllProducts = () => {
         <option value="">Sort By Price: High To Low</option>
       </select>
       <div className="grid grid-cols-12 gap-12 items-center">
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
-        <ProductBox />
+        {products.map((product) => (
+          <ProductBox key={product._id} {...product} />
+        ))}
       </div>
     </div>
   );
 };
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  connectToDB();
+  const products = await ProductModel.find({});
+
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+    revalidate: 60 * 60 * 12,
+  };
+};
+
 export default AllProducts;
